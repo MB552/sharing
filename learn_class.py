51d104b03d54a2917954ba2f2ec8d2f29282a415
MB552 <39552849+MB552@users.noreply.github.com>
2020-10-07 11:06:15 +0100

    Add files via upload

---
+++ learn_class.py
@@ -0,0 +1,16 @@
+class Dog:
+    
+    species = "Canis"
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+        
+Ralph = Dog("Ralph", 4)
+
+Lance = Dog("Lance", 9)
+
+
+print (Lance.age)
+
